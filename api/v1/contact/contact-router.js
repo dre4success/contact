bd714de5b84a648e8f72443942d96be0cdd81cae
Damilare Joseph <dre4success@live.com>
2017-07-02 21:31:13 +0100

    router

---
+++ api/v1/contact/contact-router.js
@@ -0,0 +1,18 @@
+const express  		= require('express'),
+	  router        = express.Router(),
+	  controller    = require('./contact-controller');
+
+
+	  router.param("id", controller.interceptIDs);
+
+	  router.route('/')
+	  	.get(controller.fetchAllContact)
+	  	.post(controller.addContact);
+
+	  router.route('/:id')
+	  	.get(controller.getContact)
+	  	.delete(controller.removeContact)
+	  	.put(controller.updateContact);
+
+
+	  module.exports = router;
